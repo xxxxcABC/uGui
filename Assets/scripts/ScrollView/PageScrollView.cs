ef553b124e540f4b0bd75579bdc371751ca55159
xxxxcABC <93467250+xxxxcABC@users.noreply.github.com>
2021-11-16 22:38:47 +0800

    11.16 22:06
    feat(scripts/ScrollView/PageScrollView):新增手动滚动，会自动定位到最近页面。

--- Assets/scripts/ScrollView/PageScrollView.cs
+++ Assets/scripts/ScrollView/PageScrollView.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PageScrollView : MonoBehaviour
+public class PageScrollView : MonoBehaviour, IEndDragHandler,IBeginDragHandler
 {
+    #region 成员变量
     private ScrollRect rect;
     private RectTransform content;
     private int pagesCount;
@@ -19,20 +21,13 @@ public class PageScrollView : MonoBehaviour
     private bool isAtuoMoving=true;
     private float autoTimer = 0;
     private float autoScrollTime = 2f;
+    private bool isDruging;
+    #endregion
     // Start is called before the first frame update
+    #region unity回调
     void Start()
     {
-        rect = GetComponent<ScrollRect>();
-        if (rect == null) {
-            throw new System.Exception("未找到ScrollRect");
-        }
-        content = transform.Find("Viewport/Content").GetComponent<RectTransform>();
-        pagesCount = content.childCount;
-        pages = new float[pagesCount];
-        for(int i = 0; i < pages.Length; i++)
-        {
-            pages[i] = (i * (1.0f / ((float)pagesCount - 1.0f)));
-        }
+        Initi();
     }
 
     // Update is called once per frame
@@ -40,6 +35,39 @@ public class PageScrollView : MonoBehaviour
     {
         MoveListener();
         AutoMoveListener();
+    }
+    //滚动到最近的页面
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        ScrollToPage(CaculateMinPage());
+        isDruging = false;
+        autoTimer = 0;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isDruging = true;
+    }
+    #endregion
+
+    #region 方法
+    //初始化
+    private void Initi() {
+        rect = GetComponent<ScrollRect>();
+        if (rect == null)
+        {
+            throw new System.Exception("未找到ScrollRect");
+        }
+        content = transform.Find("Viewport/Content").GetComponent<RectTransform>();
+        pagesCount = content.childCount;
+        if (pagesCount == 1)
+        {
+            throw new System.Exception("只有一个页面");
+        }
+        pages = new float[pagesCount];
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i] = (i * (1.0f / ((float)pagesCount - 1.0f)));
+        }
     }
     //监听滚动
     private void MoveListener() {
@@ -56,6 +84,9 @@ public class PageScrollView : MonoBehaviour
     //监听自动滚动
     private void AutoMoveListener()
     {
+        if (isDruging) {
+            return;
+        }
         if (isAtuoMoving)
         {
             autoTimer += Time.deltaTime;
@@ -74,4 +105,14 @@ public class PageScrollView : MonoBehaviour
         timer = 0;
         startPosition = rect.horizontalNormalizedPosition;
     }
+    //计算离的最近的页面
+    private int CaculateMinPage() {
+        int minPage = 0;
+        for (int i = 0; i < pages.Length; i++)
+        {
+            minPage = (Mathf.Abs(pages[i] - rect.horizontalNormalizedPosition) < Mathf.Abs(pages[minPage] - rect.horizontalNormalizedPosition)) ? i : minPage;
+        }
+        return minPage;
+    }
+    #endregion
 }
